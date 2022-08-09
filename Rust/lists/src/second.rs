f28c0f0ad31682c97915e9250ab9c7c15ee3b334
Anthony K. Mwangi <68157384+raskovnik@users.noreply.github.com>
2022-08-09 16:26:00 +0300

    iter

--- Rust/lists/src/second.rs
+++ Rust/lists/src/second.rs
@@ -2,6 +2,10 @@ pub struct List<T> {
     head: Link<T>,
 }
 
+pub struct Iter<'a, T> {
+    next: Option<&'a Node<T>>,
+}
+
 type Link<T> = Option<Box<Node<T>>>;
 
 struct Node<T> {
@@ -39,6 +43,10 @@ impl<T> List<T> {
     pub fn into_iter(self) -> IntoIter<T> {
         IntoIter(self)
     }
+
+    pub fn iter<'a>(&'a self) -> Iter<'a, T> {
+        Iter {next: self.head.as_deref()}
+    }
 }
 
 impl<T> Drop for List<T> {
@@ -50,6 +58,16 @@ impl<T> Drop for List<T> {
     }
 }
 
+impl<'a, T> Iterator for Iter<'a, T> {
+    type Item = &'a T;
+    
+    fn next(&mut self) -> Option<Self::Item> {
+        self.next.map(|node| {
+            self.next = node.next.as_deref();
+            &node.elem
+        })
+    }
+}
 
 pub struct IntoIter<T>(List<T>);
 
@@ -92,5 +110,21 @@ mod test{
         assert_eq!(iter.next(), Some(2));
         assert_eq!(iter.next(), Some(1));
         assert_eq!(iter.next(), None);
+    }
+
+    #[test]
+    fn iter() {
+        let mut list = List::new();
+
+        list.push(1);
+        list.push(2);
+        list.push(3);
+
+        let mut iter = list.iter();
+        assert_eq!(iter.next(), Some(&3));
+        assert_eq!(iter.next(), Some(&2));
+        assert_eq!(iter.next(), Some(&1));
+        assert_eq!(iter.next(), None);
+        
     }
 }
